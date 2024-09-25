846c7139528a3da019a1a4fa11491ca28c6e98b7
CastroNicolas <maximonicolascastro@gmail.com>
2024-09-25 22:10:53 +0200

    .env add

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore/lite";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlT0MzmD5Z_AzuXQCFgZz6ICF-E_OrA-4",
-  authDomain: "journalapp-c20a7.firebaseapp.com",
-  projectId: "journalapp-c20a7",
-  storageBucket: "journalapp-c20a7.appspot.com",
-  messagingSenderId: "141747487107",
-  appId: "1:141747487107:web:2668f95108e22e8208f6b6",
-  measurementId: "G-QPGBG0FMRR",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
